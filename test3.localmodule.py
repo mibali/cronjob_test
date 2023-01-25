2f9ae43091006466110ec0d093c5b9c3c980d202
mibali <111276665+mibali@users.noreply.github.com>
2023-01-25 18:01:58 +0000

    Create test3.localmodule.py

---
+++ test3.localmodule.py
@@ -0,0 +1,20 @@
+{
+    "moniker": {
+        "definition": "definition1",
+        "part_of_speech": "cluster",
+        "synonyms": ["synonym1", "synonym2", "synonym3"],
+        "example_sentence": "This is an example sentence using word1."
+    },
+    "word2": {
+        "definition": "definition2",
+        "part_of_speech": "<!cluster>",
+        "synonyms": ["synonym4", "synonym5", "synonym6"],
+        "example_sentence": "This is an example sentence using word2."
+    },
+    "word3": {
+        "definition": "definition3",
+        "part_of_speech": "adjective",
+        "synonyms": ["synonym7", "synonym8", "synonym9"],
+        "example_sentence": "This is an example sentence using word3."
+    }
+}
