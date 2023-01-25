8881904a523eb3e06a2f82829a78bffa23493b23
mibali <111276665+mibali@users.noreply.github.com>
2023-01-25 17:44:40 +0000

    Create test1.localmodule.py

---
+++ test1.localmodule.py
@@ -0,0 +1,20 @@
+{
+    "word1": {
+        "definition": "definition1",
+        "part_of_speech": "noun",
+        "synonyms": ["synonym1", "synonym2", "synonym3"],
+        "example_sentence": "This is an example sentence using word1."
+    },
+    "word2": {
+        "definition": "definition2",
+        "part_of_speech": "cluster",
+        "synonyms": ["synonym4", "synonym5", "synonym6"],
+        "example_sentence": "This is an example sentence using word2."
+    },
+    "word3": {
+        "definition": "definition3",
+        "part_of_speech": "adjective",
+        "synonyms": ["synonym7", "synonym8", "synonym9"],
+        "example_sentence": "This is an example sentence using word3."
+    }
+}
